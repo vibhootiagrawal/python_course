615e2372ea6d631798e56f644618453d7af1b663
vibhootiagrawal <34835078+vibhootiagrawal@users.noreply.github.com>
2019-03-15 10:57:14 +0530

    update

---
+++ space_numpy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 23:41:26 2019
+
+@author: Education
+"""
+# program to convert number into 3X3 shape array and reshap it
+
+#import numpy
+import numpy as np
+
+array = np.array([[6,9,2,3,5,8,1,5,4]]) #2-D array
+
+reshape = array.reshape(3,3)  #reshape array 3X3 array
